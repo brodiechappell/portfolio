754a3f3bb9740a2ce19e7e620ad89aaf0d5d0fc4
brodiechappell <brodiechappell123@gmail.com>
2026-09-01 11:38:18 +0100

    Delete includes directory

--- includes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <p class="footer-text">
-        UN Sustainable Development Goals (SDGs) — particularly Goal 11: Sustainable Cities and Communities, Goal 12: Responsible Consumption and Production, and Goal 15: Life on Land.
-    </p>
-</footer>
